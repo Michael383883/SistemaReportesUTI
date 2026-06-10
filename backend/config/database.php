44e18c7bca8f8f399474c35e0adc1b4603faf3df
Michael Asencio <164639782+Michael383883@users.noreply.github.com>
2026-06-10 13:06:05 -0400

    feat: configuracion de docker compose

--- backend/config/database.php
+++ backend/config/database.php
@@ -38,6 +38,8 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
+            'encrypt' => 'no',
+            'trust_server_certificate' => true,
         ],
 
     ],
